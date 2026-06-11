68746f65f50c56e8fbc290d2bc6348c530626cee
swapnakota31 <swapnakota3031@gmail.com>
2026-06-11 14:22:57 +0530

    changes cloudinary

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -6,5 +6,8 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 })
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME)
+console.log("API Key:", process.env.CLOUDINARY_API_KEY)
+console.log("Secret Exists:", !!process.env.CLOUDINARY_API_SECRET)
 
 export default cloudinary
